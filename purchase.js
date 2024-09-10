1405926680c1e42855a5744294dce9be334e3afe
Anshul Sharma <anshul.sharma6163@gmail.com>
2024-09-10 21:44:02 +0530

    new dashboard pages creation

--- purchase.js
+++ purchase.js
@@ -360,3 +360,51 @@ function previewImage(file) {
         });
     });
     
+// ---------------------------------------checkout------------------------
+
+const prices = [24.00, 24.00, 24.00];
+const quantities = [10, 12, 8];
+
+function updateSubTotal(index) {
+    const qty = parseInt(document.getElementById(`qty-${index}`).value);
+    const subtotal = prices[index] * qty;
+    document.getElementById(`subtotal-${index}`).textContent = `$${subtotal.toFixed(2)}`;
+    updateTotal();
+}
+
+function updateTotal() {
+    let total = 0;
+    for (let i = 0; i < quantities.length; i++) {
+        total += prices[i] * parseInt(document.getElementById(`qty-${i}`).value);
+    }
+    document.getElementById('total').textContent = `$${total.toFixed(2)}`;
+}
+
+function increaseQty(index) {
+    const qtyInput = document.getElementById(`qty-${index}`);
+    let qty = parseInt(qtyInput.value);
+    qty++;
+    qtyInput.value = qty;
+    updateSubTotal(index);
+}
+
+function decreaseQty(index) {
+    const qtyInput = document.getElementById(`qty-${index}`);
+    let qty = parseInt(qtyInput.value);
+    if (qty > 1) {
+        qty--;
+        qtyInput.value = qty;
+        updateSubTotal(index);
+    }
+}
+
+
+// ------------------ payment section  -----------------------
+document.querySelectorAll('input[name="payment-method"]').forEach((input) => {
+    input.addEventListener('change', function() {
+        const selectedMethod = this.value;
+        console.log('Selected payment method:', selectedMethod);
+        // Add your logic here for switching between payment methods
+    });
+});
+
